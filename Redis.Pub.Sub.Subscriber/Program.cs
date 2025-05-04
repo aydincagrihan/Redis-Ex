50972578702526cb6beaed104b3e6a9384943582
Çağrıhan Aydın <cagrihanaydin@gmail.com>
2025-05-04 15:45:51 +0300

    pub/sub

---
+++ Redis.Pub.Sub.Subscriber/Program.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});//dinleyici
+
+Console.Read();//console kapanmaması için sabitliyoruz.
--- Redis.Pub.Sub/Program.cs
+++ Redis.Pub.Sub/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
 Console.WriteLine("Hello, World!");
+
+
+ConnectionMultiplexer connection=await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber= connection.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj : ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+
+}
